962123a0f3966712a79bcedea76a849df7a02932
Moritz_Hartmann <W8w6V35zC3q2A7mQyhMD>
2023-03-25 23:16:52 +0100

    feat: ManeuverManager.cs

---
+++ sources/Models/ManeuverManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace K2D2.sources.Models
+{
+    public class ManeuverManager
+    {
+        private FunctionQueue _customQueue;
+        
+        public ManeuverManager()
+        {
+            _customQueue = new FunctionQueue();
+            
+        }
+        
+        public void AddManeuver(Delegate innerfunc, params object[] parameters)
+        {
+            _customQueue.Add(innerfunc, parameters);
+        }
+        
+        
+        public void StartManeuver()
+        {
+            if (_customQueue.HasNext())
+            {
+                _customQueue.Pop();
+                return;
+            }
+        }
+        
+    }
+}
